ce0530fb79159622597576a59f50d498498d9219
kaeyukari <karenyukariyt19@gmail.com>
2025-10-22 11:09:23 +0000

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname))); // 🔥 Serve o HTML
 
 // 🔗 Conexão com banco no Render
 const pool = new Pool({
-  connectionString: "postgresql://bezkoder_crud_user:kDyPGPdBwC46CrFTtSClAmr9csVvk8IB@dpg-d3s9o4buibrs73ep8vc0-a/bezkoder_crud",
+  connectionString: "postgresql://bancocrud_user:uICImAhKk3gqD2aSQxOl5zbHUmJLVDXm@dpg-d3sbl5gdl3ps73d9a3n0-a/bancocrud",
   ssl: { rejectUnauthorized: false }
 });
 
